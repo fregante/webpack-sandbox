c3cace2447efbd0175ee536b674ce3324ae0f825
Federico <me@fregante.com>
2025-05-15 01:40:55 +0800

    `tree-shaking-imports` - add multiple styles

--- tree-shaking-exports/main.js
+++ tree-shaking-exports/main.js
@@ -1,7 +1,17 @@
-import { DEBUG } from './consts.js';
+import { PACKAGED } from 'tree-shaking-exports';
+import { SINGLE } from './imports/single.js';
+import * as star from './imports/star.js';
 
 console.log('sup');
 
-if (DEBUG) {
-    console.log('DEBUG is true');
+if (star.STAR) {
+    console.log('`import * as star` not tree-shaken');
+}
+
+if (SINGLE) {
+    console.log('`import { SINGLE }` not tree-shaken');
+}
+
+if (PACKAGED) {
+    console.log('`import from "name"` not tree-shaken');
 }
